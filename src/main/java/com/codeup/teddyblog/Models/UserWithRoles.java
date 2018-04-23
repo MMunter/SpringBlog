a036c16365535243347744064bc16fcefd08c7c4
mdm911 <mdm911@yahoo.com>
2018-04-23 17:06:40 -0500

    In progress security exercise

--- src/main/java/com/codeup/teddyblog/Models/UserWithRoles.java
+++ src/main/java/com/codeup/teddyblog/Models/UserWithRoles.java
@@ -2,10 +2,11 @@ package com.codeup.teddyblog.Models;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public class UserWithRoles extends User implements UserDetails{
+public class UserWithRoles extends User implements UserDetails {
 
     public UserWithRoles(User user) {
         super(user);
